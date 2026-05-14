a58a104f19d524ad19ec4e7bcc6290873bf70378
anamoo <amoorillon@itdurango.edu.mx>
2026-05-13 20:56:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=["https://tecnobot.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"], 
 )
 
 @app.post("/chat")
